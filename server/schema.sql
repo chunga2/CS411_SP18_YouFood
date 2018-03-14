090a8835980dca4b5999308bd0ae2f21ed766514
Rohit Singh <singhrohit2@hotmail.com>
2018-03-14 12:33:51 -0500

    Fix table name quotations

--- server/schema.sql
+++ server/schema.sql
@@ -1,35 +1,35 @@
-CREATE TABLE Owner(
+CREATE TABLE "Owner"(
     email citext
         CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$') PRIMARY KEY,
     name text NOT NULL,
     hashedpass char(20) NOT NULL
 );
-CREATE TABLE User(
+CREATE TABLE "User"(
     email citext
         CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$') PRIMARY KEY,
     name text NOT NULL,
     hashedpass char(20) NOT NULL
 );
-CREATE TABLE Budget(
+CREATE TABLE "Budget"(
     date timetz NOT NULL,
     useremail REFERENCES User(email),
     total money NOT NULL,
     PRIMARY KEY (useremail, startdate)
 );
-CREATE TABLE Transaction(
+CREATE TABLE "Transaction"(
     date timetz NOT NULL,
     useremail REFERENCES User(email),
     amount money NOT NULL,
     PRIMARY KEY (useremail, date)
 )
 
-CREATE TABLE Recommendation(
+CREATE TABLE "Recommendation"(
     date timetz NOT NULL,
     useremail REFERENCES User(email),
     restaurant REFERENCES Restaurant(address),
     PRIMARY KEY 
 );
-CREATE TABLE Review(
+CREATE TABLE "Review"(
     useremail REFERENCES User(email),
     restaurant REFERENCES Restaurant(address),
     description text,
@@ -37,10 +37,10 @@ CREATE TABLE Review(
         CONSTRAINT onetoten CHECK (rating <= 10 AND rating >= 0) NOT NULL,
     date timetz NOT NULL
 )
-CREATE TABLE Promotion(
+CREATE TABLE "Promotion"(
     restaurant REFERENCES Restaurant(address),
 )
-CREATE TABLE Restaurant(
+CREATE TABLE "Restaurant"(
     address text NOT NULL PRIMARY KEY,
     pricerange int
         CONSTRAINT numdollarsigns CHECK(pricerange <=4 AND pricerange >=1) NOT NULL,
