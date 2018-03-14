03130c5b6237c88dd7a3909134cba412284692ab
Rohit Singh <singhrohit2@hotmail.com>
2018-03-14 12:05:56 -0500

    Add server credentials

---
+++ server/credentials.py
@@ -0,0 +1,7 @@
+# This file contains information about the database server config.
+# The database was set up with the following command:
+# CREATE USER youfood WITH PASSWORD 'wizard11'; CREATE DATABASE youfood; GRANT ALL PRIVILEGES ON DATABASE youfood TO youfood;
+USER = 'youfood'
+PASSWORD = 'wizard11'
+HOST = 'localhost'
+DATABASE = 'youfood'
