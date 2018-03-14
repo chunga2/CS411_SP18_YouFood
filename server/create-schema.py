5a4fc8a0e98f5ad52216773d54c537b8b5485313
Rohit Singh <singhrohit2@hotmail.com>
2018-03-14 12:56:50 -0500

    Set up tables

--- server/create-schema.py
+++ server/create-schema.py
@@ -3,5 +3,7 @@ import psycopg2
 
 conn = psycopg2.connect(dbname=DATABASE, user=USER, password=PASSWORD, host=HOST)
 with open("schema.sql") as f:
+    print("Opened SQL script...")
     with conn.cursor() as cur:
+        print("Opened database connection...")
         cur.execute(f.read())
