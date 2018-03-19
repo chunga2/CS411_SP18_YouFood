e7d401456494646ffeaddc0ce2722f39e7243920
Raajesh12 <raajstar3@gmail.com>
2018-03-19 18:01:38 -0500

    Added post and put

--- youfood_api/api.py
+++ youfood_api/api.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import IntegrityError
 from flask import Flask, jsonify, request, Response
 from flask.views import MethodView
 
@@ -11,31 +12,102 @@ class UserAPI(MethodView):
     
     def get(self):
         """
-        /users?email=<emali>
+        GET: /users?email=<emali>
         Gets user info for a given specified email address that is a URL query parameter
         """
-
         cur = conn.cursor()
         email = str(request.args.get("email"))
-        print email
         cur.execute("SELECT * FROM \"User\" WHERE email=%s;", (email,))
 
         row = cur.fetchone()
         # Executes if no user with that email address was found
         if row == None:
-            response = Response(status=400)
-            return response
+            conn.rollback()
+            cur.close()
+            return jsonify({'error': 'No user with that email exists'}), 400
 
         data = {
-            'Email' : row[0],
-            'Name' : row[1]
+            'email' : row[0],
+            'name' : row[1],
+            'password' : row[2]
         }
 
         cur.close()
         return jsonify(data), 200
 
+    def post(self):
+        """
+        POST: /users
+
+        JSON request payload:
+        {
+            "email" : <email>
+            "name" : <name>
+            "password" : <password>
+        }
+        """
+        json_data = request.get_json()
+        email = json_data.get("email")
+        name = json_data.get("name")
+        password = json_data.get("password")
+        
+        if email == None or name == None or password == None:
+            return Response(status=400)
+
+        cur = conn.cursor()
+        try:
+            cur.execute("INSERT INTO \"User\" (email, name, hashedpass) VALUES (%s, %s, %s);", (email, name, password))
+            conn.commit()
+            cur.close()
+        except IntegrityError:
+            conn.rollback()
+            cur.close()
+            return jsonify({'error': 'User with email already exists'}), 400
+
+        cur.close()
+        return Response(status=201)
+
+    def put(self):
+        """
+        PUT: /users
+
+        JSON request payload (email is required, at least 1 of name or password is also required)
+        {
+            "email" : <email>
+            "name" : <name>
+            "password" : <password>
+        }
+        """
+        json_data = request.get_json()
+        email = json_data.get("email")
+        name = json_data.get("name")
+        password = json_data.get("password")
+        
+        if email == None or (name == None and password == None):
+            return jsonify({'error': 'Missing email or both name and password'}), 400
+
+        cur = conn.cursor()
+        if(name != None):
+            cur.execute("UPDATE \"User\" SET name = %s WHERE email = %s;", (name, email))
+            if cur.rowcount == 0:
+                conn.rollback()
+                cur.close()
+                return jsonify({'error': 'No user with that email exists'}), 400
+
+        if(password != None):
+            cur.execute("UPDATE \"User\" SET hashedpass = %s WHERE email = %s;", (password, email))
+            if cur.rowcount == 0:
+                conn.rollback()
+                cur.close()
+                return jsonify({'error': 'No user with that email exists'}), 400
+
+        conn.commit()
+        cur.close()
+        return Response(status=204)
+
+
 user_view = UserAPI.as_view('user_api')
-app.add_url_rule('/user', view_func=user_view, methods=['GET'])
+app.add_url_rule('/users', view_func=user_view, methods=['GET', 'POST', 'PUT'])
 
 
 if __name__ == "__main__":
