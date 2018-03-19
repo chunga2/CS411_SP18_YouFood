3e4b891d3e0e98e416b2bc709f161b2b7a2ace39
Raajesh12 <raajstar3@gmail.com>
2018-03-19 20:07:42 +0000

    Added user get

--- youfood_api/api.py
+++ youfood_api/api.py
@@ -1,17 +1,42 @@
 import psycopg2
-from flask import Flask
+from flask import Flask, jsonify, request, Response
+from flask.views import MethodView
+
+
 app = Flask(__name__)
 
 conn = psycopg2.connect(dbname="youfood", user="youfood", password="wizard11", host="localhost")
 
-@app.route("/")
-def hello():
+class UserAPI(MethodView):
+    
+    def get(self):
+        """
+        /users?email=<emali>
+        Gets user info for a given specified email address that is a URL query parameter
+        """
+
         cur = conn.cursor()
-        cur.execute("SELECT * FROM \"User\"") 
-        rows = cur.fetchall()
-        email = rows[0][0]
+        email = str(request.args.get("email"))
+        print email
+        cur.execute("SELECT * FROM \"User\" WHERE email=%s;", (email,))
+
+        row = cur.fetchone()
+        # Executes if no user with that email address was found
+        if row == None:
+            response = Response(status=400)
+            return response
+
+        data = {
+            'Email' : row[0],
+            'Name' : row[1]
+        }
+
         cur.close()
-        return email
+        return jsonify(data), 200
+
+user_view = UserAPI.as_view('user_api')
+app.add_url_rule('/user', view_func=user_view, methods=['GET'])
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
