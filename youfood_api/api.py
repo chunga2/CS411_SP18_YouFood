d3c46e0efc693cf6aec5ff288efde8f120604e2a
Raajesh12 <raajstar3@gmail.com>
2018-03-19 18:19:19 +0000

    connected to postgres and sample query

--- youfood_api/api.py
+++ youfood_api/api.py
@@ -1,9 +1,17 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
+conn = psycopg2.connect(dbname="youfood", user="youfood", password="wizard11", host="localhost")
+
 @app.route("/")
 def hello():
-        return "<h1 style='color:blue'>Hello World!</h1>"
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM \"User\"") 
+        rows = cur.fetchall()
+        email = rows[0][0]
+        cur.close()
+        return email
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
