e4fe08225a7ec9a74e11886e28b7140624da91db
Raajesh12 <raajstar3@gmail.com>
2018-03-21 21:24:27 -0500

    Added response code constants and default network error message

---
+++ youfood_android_app/app/src/main/java/com/cs411/droptableuser/youfood_android_app/ResponseCodes.java
@@ -0,0 +1,11 @@
+package com.cs411.droptableuser.youfood_android_app;
+
+/**
+ * Created by raajesharunachalam on 3/21/18.
+ */
+
+public class ResponseCodes {
+    public static final int HTTP_OK = 200;
+    public static final int HTTP_NO_RESPONSE = 204;
+    public static final int HTTP_CREATED = 201;
+}
