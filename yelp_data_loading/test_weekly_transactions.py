9d250875ada59ea559c39a17ed0065c29c8ff032
Raajesh12 <raajstar3@gmail.com>
2018-03-23 19:24:23 -0500

    Updated test script

--- yelp_data_loading/test_weekly_transactions.py
+++ yelp_data_loading/test_weekly_transactions.py
@@ -12,16 +12,18 @@ cur = conn.cursor()
 #in the API, probbaly parse from a JSON the user inputs.
 #How are we integrating the graphing?
 #Like how will the user access it and use it
-dt = datetime.now();
+date = '3-04-2016 0:0:0'
+useremail = 'raajstar3@gmail.com'
+dt = datetime.strptime(date, "%d-%m-%Y %H:%M:%S");
 
 #execute the thing
 cur.execute('''
-SELECT *
+SELECT COUNT(*)
 FROM "Transaction"
-WHERE date < (%s::date - (concat((MOD(extract(isodow FROM %s)::int, 7)),' days'))::interval+'7 days'::interval) AND
+WHERE useremail=%s AND date < (%s::date - (concat((MOD(extract(isodow FROM %s)::int, 7)),' days'))::interval+'7 days'::interval) AND
 date > (%s::date - (concat((MOD(extract(isodow FROM %s)::int, 7)),' days'))::interval);
 ''',
-(dt,))
+(useremail, dt,dt,dt,dt))
 
 #All Goes well, you should see something here.
 #IDK what though, probably just a bunch of characters and stuff. It's unformated
